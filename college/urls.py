0430c13f23de89c7b5282f51251fde422058e9e1
Countmooshroom <93834465+Countmooshroom@users.noreply.github.com>
2022-12-01 08:21:16 -0700

    Add files via upload

---
+++ college/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from college import views
+
+app_name = 'college'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
