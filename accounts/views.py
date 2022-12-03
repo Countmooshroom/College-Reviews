194241c3d56851722e2a9d16756bd73b4955e10d
Ky Fike <kyfike@gmail.com>
2022-12-03 14:14:54 -0700

    confirm user information

--- accounts/views.py
+++ accounts/views.py
@@ -11,10 +11,18 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-        user.save();
-        print('user created')
-        return redirect('/')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print('That username is already taken!')
+            elif User.objects.filter(email=email).exists():
+                print('That email is linked to another account!')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save();
+                print('user created')
+                return redirect('/')
+        else:
+            print("You're passwords don't match!")
 
     else:
         return render(request, 'college/register.html')
