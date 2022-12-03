2976f1ba2877aeef1abc86a04cb899772d903450
Ky Fike <kyfike@gmail.com>
2022-12-03 13:27:35 -0700

    create app for user registration

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path("register", views.register, name="register"),
+]
--- college_recommend/urls.py
+++ college_recommend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("college.urls")),
-    path('admin/', admin.site.urls)
+    path('', include('college.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
