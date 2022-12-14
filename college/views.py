a6f065b7150681af02ceeaa3d5f946382625a5c3
Ky Fike <kyfike@gmail.com>
2022-12-13 20:47:16 -0700

    merge error fix

--- college/views.py
+++ college/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-from .models import Choice, Course, Question
+from .models import Choice, Course
 
 
 def index(request):
